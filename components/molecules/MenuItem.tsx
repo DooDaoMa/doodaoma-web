6c2253f0da9cd45e72f3b58cc86547f8e7086f83
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-01-14 21:56:56 +0700

    add text color to MenuItem

--- components/molecules/MenuItem.tsx
+++ components/molecules/MenuItem.tsx
@@ -12,7 +12,10 @@ export const MenuItem = (props: MenuItemProps) => {
   const { icon, to } = props
   return (
     <Link href={to}>
-      <FontAwesomeIcon icon={icon} className="nav-icon" />
+      <FontAwesomeIcon
+        icon={icon}
+        className="cursor-pointer text-blue-600 sm:h-3 sm:h-3 md:h-5 md:w-5"
+      />
     </Link>
   )
 }
