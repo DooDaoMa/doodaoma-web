c37ff63fcc4e17e32783f1b635b0e762396e44a5
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-01-22 22:01:25 +0700

    add Section

---
+++ components/organisms/Section.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react'
+
+type SectionProps = {
+  children: ReactNode
+  className?: string
+}
+
+export const Section = (props: SectionProps) => {
+  const { children, className, ...rest } = props
+  return (
+    <section
+      className={`rounded rounded-md border border-gray-200 px-12 py-8 shadow-sm ${className}`}
+      {...rest}>
+      {children}
+    </section>
+  )
+}
