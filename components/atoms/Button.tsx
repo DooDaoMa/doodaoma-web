bdd943924526d7546d0aebd9238c507a103f2067
Nice <nicenicegame@gmail.com>
2023-02-02 09:53:28 +0700

    fix button props

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -1,17 +1,16 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+
 type ButtonProps = {
-  children: string
-  type?: 'button' | 'submit' | 'reset'
-  style?: string
-  onClick?: () => void
-}
+  children: ReactNode
+  type: 'button' | 'submit' | 'reset'
+} & ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = (props: ButtonProps) => {
-  const { children, type = 'button', ...rest } = props
+  const { children, type = 'button' } = props
   return (
     <button
       type={type}
-      className="block w-full rounded rounded-md border border-gray-300 bg-blue-500 px-3 py-1 text-white focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-      {...rest}>
+      className="block w-full rounded rounded-md border border-gray-300 bg-blue-500 px-3 py-1 text-white focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
       {children}
     </button>
   )
