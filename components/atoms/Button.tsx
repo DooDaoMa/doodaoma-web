ab93a2c09a368da0f73f5bc6064f8dc96cb95e6e
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-02-28 18:12:58 +0700

    style button

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -2,16 +2,17 @@ import { ButtonHTMLAttributes, ReactNode } from 'react'
 
 type ButtonProps = {
   children: ReactNode
-  type: 'button' | 'submit' | 'reset'
+  type?: 'button' | 'submit' | 'reset'
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = (props: ButtonProps) => {
-  const { children, type = 'button' } = props
+  const { children, type = 'button', onClick, className } = props
 
   return (
     <button
       type={type}
-      className="transition-button block w-fit rounded rounded-md border border-gray-300 bg-blue-500 px-4 py-2 font-bold uppercase text-white transition focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+      className={`transition-button block w-fit rounded-md border border-gray-300 bg-blue-500 px-4 py-2 font-bold uppercase text-white transition focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm ${className}`}
+      onClick={onClick}>
       {children}
     </button>
   )
