5d32a3e320e39974fce2ccab000e52e94082b1f6
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-02-28 18:14:44 +0700

    set ReactModal teleport dest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ReactModal from 'react-modal'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -8,6 +9,7 @@ import { Layout } from '../components'
 import { store } from '../store'
 
 export default function App({ Component, pageProps }: AppProps) {
+  ReactModal.setAppElement('#__next')
   return (
     <Provider store={store}>
       <Layout>
