80745b692ba9943e1d395c0c2a9766712359f320
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-03-14 17:48:41 +0700

    update type

--- types/reservation.ts
+++ types/reservation.ts
@@ -1,4 +1,19 @@
 export type IReservation = {
+  username: string
+  deviceId: string
   startTime: string
   endTime: string
+  status: 'available' | 'reserved'
+}
+
+export type AvailableReservationQueryParams = {
+  startDate: string
+  endDate: string
+}
+
+export type ITimeSlot = {
+  startTime: Date
+  endTime: Date
+  username: string
+  status: string
 }
