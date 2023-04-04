03f3aa1b34b504c21c8949c6f48bf0dbf0f8f6f4
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-04-04 16:33:25 +0700

    remove dup css class

--- components/organisms/Section.tsx
+++ components/organisms/Section.tsx
@@ -9,7 +9,7 @@ export const Section = (props: SectionProps) => {
   const { children, className, ...rest } = props
   return (
     <section
-      className={`rounded rounded-md border border-gray-200 px-12 py-8 shadow-sm ${className}`}
+      className={`rounded-md border border-gray-200 px-12 py-8 shadow-sm ${className}`}
       {...rest}>
       {children}
     </section>
