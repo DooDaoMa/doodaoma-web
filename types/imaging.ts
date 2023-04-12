514c13fb6b405f0f17e9b7ba86f0ef79d1a9cedd
nicenicegame <nicenicegame@gmail.com>
2023-04-12 18:28:46 +0700

    Squashed commit of the following:
    commit b82e8dabbfc57e1ad6d7077361868070ada782b5
    Author: Nice <nicenicegame@gmail.com>
    Date:   Wed Apr 12 18:04:58 2023 +0700
    
        wip update required fields
    
    commit 4a5c16fdebb67dfad6054822386d2737c76fa686
    Author: Nice <nicenicegame@gmail.com>
    Date:   Wed Mar 22 12:53:45 2023 +0700
    
        rename imaging selector
    
    commit 2c70b32d3cd62d3043a9389aabb9da96c652eb87
    Merge: 50cf014 bd69773
    Author: Nice <nicenicegame@gmail.com>
    Date:   Wed Mar 22 12:49:50 2023 +0700
    
        Merge branch 'feauture/connect-ws' of https://github.com/DooDaoMa/doodaoma-web into feauture/connect-ws
    
    commit 50cf0143bafa056e4f123c7e198bee7cc5fd718f
    Author: nicenicegame <nicenicegame@gmail.com>
    Date:   Tue Mar 21 20:54:54 2023 +0700
    
        wip imaging form
    
    commit 569e5d163c94a2b5b337627da6e56ad18f18e4e6
    Author: Nice <nicenicegame@gmail.com>
    Date:   Tue Mar 14 14:41:21 2023 +0700
    
        add ws hook
    
    commit bd69773656997beb76d02da742e878263148a511
    Author: nicenicegame <nicenicegame@gmail.com>
    Date:   Tue Mar 21 20:54:54 2023 +0700
    
        wip imaging form
    
    commit 933107f76f8bd3ef1d76427d483593c98c1565ea
    Merge: e47352a c6d3af1
    Author: nicenicegame <nicenicegame@gmail.com>
    Date:   Sun Mar 19 01:48:43 2023 +0700
    
        Merge branch 'feauture/connect-ws' of https://github.com/DooDaoMa/doodaoma-web into feauture/connect-ws
    
    commit e47352a954e12ec19271c2d5099a42093e6fa65b
    Author: nicenicegame <nicenicegame@gmail.com>
    Date:   Sun Mar 19 01:46:44 2023 +0700
    
        validate auth by requireAuth property on page component
    
    commit 58b74b151f843e1e379ef5a200b5ac65bd6c51e1
    Author: nicenicegame <nicenicegame@gmail.com>
    Date:   Sat Mar 18 16:51:48 2023 +0700
    
        wip construct imaging form
    
    commit 73c3875a599b16734aef7ff9ebd16b1a0c1a4f3c
    Author: Nice <nicenicegame@gmail.com>
    Date:   Tue Mar 14 15:20:11 2023 +0700
    
        wip connect ws
    
    commit 6f25ce306585127be75bf7b1ba377ee6862bb222
    Author: Nice <nicenicegame@gmail.com>
    Date:   Tue Mar 14 14:41:21 2023 +0700
    
        add ws hook
    
    commit c6d3af1b7bfe0326d8aefff171845f0a65c50860
    Author: Nice <nicenicegame@gmail.com>
    Date:   Tue Mar 14 15:20:11 2023 +0700
    
        wip connect ws
    
    commit 64e26f3b06b07e9fd15728f811014a1db8ede51f
    Author: Nice <nicenicegame@gmail.com>
    Date:   Tue Mar 14 14:41:21 2023 +0700
    
        add ws hook

--- types/imaging.ts
+++ types/imaging.ts
@@ -2,3 +2,87 @@ export interface FilterWheelOption {
   position: number
   name: string
 }
+
+export interface IImagingForm {
+  startSequence: {
+    cooling: {
+      temperature: number
+      duration: number
+    }
+  }
+  imagingSequence: {
+    target: {
+      name: string
+      rotation: number
+      ra: {
+        hours: number
+        minutes: number
+        seconds: number
+      }
+      dec: {
+        degrees: number
+        minutes: number
+        seconds: number
+      }
+    }
+    tracking: {
+      mode: 0 | 1 | 2 | 3 | 5
+    }
+    guiding: {
+      forceCalibration: boolean
+    }
+    exposures: {
+      gain: number
+      time: number
+      binning: string
+      imageType: string
+    }
+  }
+  endSequence: {
+    warming: {
+      duration: number
+    }
+  }
+}
+
+export const imagingFormDefaultValue: IImagingForm = {
+  startSequence: {
+    cooling: {
+      temperature: -10,
+      duration: 5,
+    },
+  },
+  imagingSequence: {
+    target: {
+      name: '',
+      rotation: 0,
+      ra: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+      dec: {
+        degrees: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+    },
+    tracking: {
+      mode: 0,
+    },
+    guiding: {
+      forceCalibration: false,
+    },
+    exposures: {
+      gain: 100,
+      time: 300,
+      binning: '1x1',
+      imageType: 'LIGHT',
+    },
+  },
+  endSequence: {
+    warming: {
+      duration: 5,
+    },
+  },
+}
