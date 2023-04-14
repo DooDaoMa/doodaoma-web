26bbaf9c9d0744f43e17cbae42196638c497ec65
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-04-14 23:10:00 +0700

    impl ForecastCard

--- components/organisms/WeatherSection.tsx
+++ components/organisms/WeatherSection.tsx
@@ -1,37 +1,25 @@
 import { format } from 'date-fns'
 import { TbSunrise, TbSunset } from 'react-icons/tb'
 
-import { ForecastData } from '../../types'
+import { ForecastData, WeatherData } from '../../types'
 import { Loading } from '../atoms/Loading'
+import { ForecastCard } from '../molecules/ForecastCard'
 
 import { Section } from './Section'
 
 type WeatherSectionProps = {
   content: ForecastData | null
+  weather: WeatherData | null
 }
 
-export const WeatherSection = ({ content }: WeatherSectionProps) => {
+export const WeatherSection = ({ content, weather }: WeatherSectionProps) => {
   return (
     <Section>
       <>
         {content ? (
           <>
             <div className="feed-card-header">Weather Today</div>
-            {/* {content?.weatherList.map((ele, i) => (
-              <div key={i} className="mb-2 rounded-md bg-slate-700">
-                <div className="flex items-center">
-                  <p>{ele.main}</p>
-                  <p>{format(ele.time, 'dd E hh:mm aaaa')}</p>
-                  <Image
-                    src={`http://openweathermap.org/img/w/${ele?.icon}.png`}
-                    width={32}
-                    height={32}
-                    alt="weather icon"
-                  />
-                </div>
-                <p className="italic">{ele?.description}</p>
-              </div>
-            ))} */}
+            {weather ? <ForecastCard data={weather} /> : <Loading />}
             <div className="flex flex-wrap gap-x-2 gap-y-2">
               <p className="flex items-center gap-x-2">
                 <TbSunset className="h-6 w-6 text-red-400" />
